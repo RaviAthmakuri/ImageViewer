9b3672326fa1fc9b9cc1d04b008d9e8af2a5212f
ravi.athmakuri <ravi.athmakuri93@gmail.com>
2020-10-19 21:10:47 +0530

    Image Viewer Completed with all changes
    Image Viewer Completed with all changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Login from './screens/login/Login';
+import Home from './screens/home/Home';
+import Profile from './screens/Profile/Profile';
+import Controller from './screens/Controller';
 
 ReactDOM.render(
     <span>
-        Image Viewer
+       {/* <Profile></Profile>  */}
+       {/* <SimpleModal></SimpleModal> */}
+       <Controller></Controller>
     </span>, 
     document.getElementById('root')
 );
